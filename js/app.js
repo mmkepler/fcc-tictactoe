c77f9b8bbe4155052be31b3a548913ac8b4ebdfa
M. Kepler <mmkepler@yahoo.com>
2016-12-20 16:55:18 -0500

    update files

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
-//https://jsfiddle.net/xb9mr7bn/9/?utm_source=website&utm_medium=embed&utm_campaign=xb9mr7bn
-//variables
+/*
+Melissa Kepler 2016, Free Code Camp Tic Tac Toe.
+*/
 
-var board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
+"use strict";
+
+/*VARIABLES*/
+
+var state = [null, null, null, null, null, null, null, null, null];
 var playerWins = 0;
 var compWins = 0;
 var playerIcon = ""; //updated when player chooses
-var compIcon = "";
-var myMove = false;
+var compIcon = ""; 
 var win = "You Win!";
 var lose = "You lose";
 var tie = "Tie Game!";
-console.log(myMove);
+var start = true;
+var compMove = false;
 
+checkForTerminal();
 
-//choice screen display
+/*Sets up game choice screen*/
+
 $("#playerScore").html(playerWins);
 $("#compScore").html(compWins);
 $("#choiceScreen").show();
@@ -27,265 +30,291 @@ $(".middle").hide();
 $("#newGame").hide();
 $("#message").hide();
 
-//moves from choice to start screen
-//adds the players chosen icon, & assigns comp icon
-function choice(icon) {
-    playerIcon = icon;
-    if(playerIcon === "X"){
-      compIcon = "O";
-    } else {
-      compIcon = "X";
-			myMove = true;
-			console.log(myMove);
-			makeMove();
-    }
-      
-    $("#choiceScreen").hide();
-    $(".inside").show();
-    $(".middle").show();
-    $(".outside").show();
-  }
-
-if (myMove) {
-    makeMove();
+/*Makes sure that computer takes it's move*/
+if (compMove) {
+  moveAI();
 }
 
-//buttons
+if(start){
+  gameStart();
+}
+
+/*FUNCTIONS*/
+
+//opens choice screen
+function gameStart(){
+  $("#playerScore").html(playerWins);
+  $("#compScore").html(compWins);
+  $("#choiceScreen").show();
+  $(".inside").hide();
+  $(".outside").hide();
+  $(".middle").hide();
+  $("#newGame").hide();
+  $("#message").hide();
+}
+
+//adds the players chosen icon, & assigns comp icon
+function choice(icon) {
+  start = false;
+  playerIcon = icon;
+  if(playerIcon === "X"){
+    compIcon = "O";
+  } else {
+    compIcon = "X";
+    compMove = true;
+    moveAI();
+  }
+  $("#choiceScreen").hide();
+  $(".inside").show();
+  $(".middle").show();
+  $(".outside").show();
+}
+
+//resets everything back to the choice screen
+function restartGame() {
+  state = [null, null, null, null, null, null, null, null, null];
+  $(".tic").prop("disabled", false).addClass("swing");
+  $(".tic").text("");
+  $(".inside").hide();
+  $(".middle").hide();
+  $(".outside").hide();
+  $("#choiceScreen").show();
+  compMove = false;
+  updateState();
+}
+
+//restarts the state, but not the player/comp icons and scores
+function resetState(){
+  $(".tic").prop("disabled", false).addClass("swing");
+  $(".tic").empty();
+  state = [null, null, null, null, null, null, null, null, null];
+  if(playerIcon === "X"){
+    compMove = false;
+  }else{
+    compMove = true;
+    moveAI();
+  }
+  $("#reset").animate({"margin-top": "24px"}, 500);
+}
+
+
+function disableTiles(){
+  $(".tic").prop("disabled", true);
+  $(".tic").removeClass("swing");
+  
+}
+
+//checks for a terminal state: win, lose, tie
+function checkForTerminal() {
+  updateState();
+    
+  let winner = winCheck(state);
+   if(winner !== null){
+     switch(winner){
+	  case 1:
+	    compWins++;
+	    disableTiles();
+         
+	    $("#compScore").text(compWins);
+	    $("#alert").html(lose);
+	    endGame();
+	    console.log("ai wins");
+	    break;
+	  case 0:
+	    playerWins++;
+	    disableTiles();
+	    $("#playerScore").html("player");
+	    $("#alert").html(win);
+	    endGame();
+	    console.log("player wins");
+	    break;
+	  case -1:
+	    $("#alert").html(tie);
+	    disableTiles();
+	    endGame();
+	    console.log("tie");
+	     break;
+	  }
+  } 
+}
+
+
+//animation that moves the newGame and reset buttons when there is a terminal state
+function endGame(){
+  $("#reset").prop("disabled", true);
+  $("#reset").animate({"margin-top": "0"},500).promise().done (function(){
+      $("#message").fadeIn(200);
+  });
+  setTimeout(function(){
+    $("#message").fadeOut(200).hide(200).promise().done(function(){
+      $("#newGame").fadeIn(600);
+      $("#reset").prop("disabled", false);
+		});
+  }, 3000);
+}
+
+
+//checks for win configurations on the state
+function winCheck(state){
+      //columns check
+      for (var i = 0; i <= 6; i = i + 3) {
+        if (state[i] !== null && state[i] === state[i + 1] && state[i + 1] === state[i + 2]) {
+          if(state[i] === false){
+              return 0;
+          } else {
+              return 1;
+          }
+        }
+      }
+    
+      //rows check
+      for (var j = 0; j <= 2; j++) {
+        if (state[j] !== null && state[j] === state[j + 3] && state[j + 3] === state[j + 6]) {
+          if(state[j] === false){
+              return 0;
+          } else {
+              return 1;
+          }
+       }
+     }
+     
+    //diagonal check
+    if(state[0] !== null && state[0] === state[4] && state[4] === state[8]){
+      if(state[0] === false){
+              return 0;
+          } else {
+              return 1;
+          }
+    }
+    
+    if(state[2] !== null && state[2] === state[4] && state[4] === state[6]){
+      if(state[2] === false){
+              return 0;
+          } else {
+              return 1;
+          }
+    }
+    
+    //check for tie
+    if ($.inArray(null, state) === -1) {
+        return -1;
+      }
+    return null;
+  }
+
+
+//checks the state array to properly display icons on the buttons
+function updateState() {
+    
+    for (var i = 0; i < 9; i++) {
+      var buttonText;
+      switch(state[i]){
+        case false:
+          buttonText = playerIcon;
+          break;
+        case true:
+          buttonText = compIcon;
+          break;
+        case null:
+          buttonText = "";
+          break
+      }
+      $("#t" + i ).text(buttonText);
+      if(state[i] !== null){
+        $("#t" + i ).prop("disabled", true).removeClass("swing");
+		
+      }
+    }	
+}
+
+
+//changes the state after the recursiveMinimax results were returned through minimax to this function
+function moveAI() {
+  state = minimax(state);
+  compMove = false;
+  checkForTerminal();
+}
+
+//calls the recursiveMinimax function and returns the results
+function minimax(state) {
+  return recursiveMinimax(state, true)[1];
+}
+
+
+//checks to see if the state is in a terminal state, and if not it calls itself until a move is chosen that will maximize the ai players chance of winning. 
+function recursiveMinimax(state, player) {
+  let winner = winCheck(state);
+  if (winner !== null) {
+    switch(winner) {
+      case 1:
+        // highest number, AI is the max player
+        return [100, state];
+								
+      case 0:
+        // lowest number, player is the min player
+        return [-100, state];
+								
+      case -1:
+        // mid number for a tie game
+        return [0, state];
+								
+      }
+    } else {
+      // sets to null initially
+      var nextValue = null;
+      var nextState = null;
+        
+        for (var i = 0; i < 9; i++) {
+          if (state[i] === null) {
+            state[i] = player;
+            //reruns with the oppoiste player(human)
+            var value = recursiveMinimax(state, !player)[0];
+            if((nextValue === null) || (player && value > nextValue) || (!player && value < nextValue) ){
+              nextState = state.slice(0);
+              nextValue = value;
+            }
+              //if the above criteria isn't met, that spot goes back to empty
+              state[i] = null;
+          }
+        }
+      
+      return [nextValue, nextState];
+  }
+}
+
+
+/*Buttons*/
+
+
 $(document).ready(function() {
 	
-    $(".tic").click(function() {
-        //$(this).prop("disabled", true);
-        var cell = $(this).attr("id");
-        var row = parseInt(cell[1]);
-        var col = parseInt(cell[2]);
-        if (!myMove) {
-            board[row][col] = false;
-            myMove = true;
-            updateMove();
-						makeMove();
-            
-        }
-			console.log("from click event " + board);
-    });
+  $(".tic").click(function() {
+    var id = $(this).attr("id");
+    var space = id[1];
+    if (!compMove) {
+      state[space] = false;
+      compMove = true;
+      checkForTerminal();
+      moveAI();     
+    }
+  });
+
+
+  $("#reset").click(function(){
+    restartGame();
+  });
 	
-    $("#reset").click(function(){
-        restartGame();
+  $("#newGame").click(function(){
+    $("#newGame").fadeOut(200).promise().done(function(){
+      resetState();
     });
-	
-	  $("#newGame").click(function(){
-			$("#newGame").fadeOut(200).promise().done(function(){
-				resetBoard();
-			});
 			
-		});
+  });
     
-    $(".choice").click(function() {
+  $(".choice").click(function() {
     choice($(this).val());
-    console.log(playerIcon);
-    console.log(compIcon);
   });
     
 });
 
-//functions
-function restartGame() {
-    board = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
-    $(".tic").prop("disabled", false);
-    $(".tic").text("");
-    $(".inside").hide();
-    $(".middle").hide();
-    $(".outside").hide();
-    $("#choiceScreen").show();
-    myMove = false;
-    updateMove();
-}
 
-function disableBoard(){
-	$(".tic").prop("disabled", true);
-}
-
-function resetBoard(){
-    $(".tic").prop("disabled", false);
-		$(".tic").empty();
-    board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-    ];
-    if(playerIcon == "X"){
-			myMove = false;
-		}else{
-			myMove = true;
-		}
-	$("#reset").animate({"margin-top": "24px"}, 500);
-}
-
-function updateMove() {
-    updateButtons();
-    
-    var winner = getWinner(board);
-    
-    
-     if(winner !== null){
-			switch(winner){
-				case 1:
-					compWins++;
-					disableBoard();
-					$("#compScore").text(compWins);
-					$("#alert").html(lose);
-					endGame();
-					console.log("ai wins");
-				  break;
-				case 0:
-					playerWins++;
-					disableBoard();
-					$("#playerScore").html("player");
-					$("#alert").html(win);
-					endGame();
-					console.log("player wins");
-					break;
-				case -1:
-					$("#alert").html(tie);
-					disableBoard();
-					endGame();
-					console.log("tie");
-					break;
-			}
-		}
-    
-}
-
-function endGame(){
-	$("#reset").animate({"margin-top": "0"},500).promise().done(function(){
-		$("#message").fadeIn(200);
-	});
-	setTimeout(function(){
-		$("#message").fadeOut(200).hide(200).promise().done(function(){
-			$("#newGame").fadeIn(600);
-		});
-	}, 3000);
-}
-
-function getWinner(board) {
-   
-    // Check if someone won
-    vals = [true, false];
-    var allNotNull = true;
-    for (var k = 0; k < vals.length; k++) {
-        var value = vals[k];
-        
-        // Check rows, columns, and diagonals
-        var diagonalComplete1 = true;
-        var diagonalComplete2 = true;
-        for (var i = 0; i < 3; i++) {
-            if (board[i][i] != value) {
-                diagonalComplete1 = false;
-            }
-            if (board[2 - i][i] != value) {
-                diagonalComplete2 = false;
-            }
-            var rowComplete = true;
-            var colComplete = true;
-            for (var j = 0; j < 3; j++) {
-                if (board[i][j] != value) {
-                    rowComplete = false;
-                }
-                if (board[j][i] != value) {
-                    colComplete = false;
-                }
-                if (board[i][j] === null) {
-                    allNotNull = false;
-                }
-            }
-            if (rowComplete || colComplete) {
-                return value ? 1 : 0;
-            }
-        }
-        if (diagonalComplete1 || diagonalComplete2) {
-            return value ? 1 : 0;
-        }
-    }
-    if (allNotNull) {
-        return -1;
-    }
-    return null;
-}
-
-//change this so it will show either playerIcon or compIcon
-function updateButtons() {
-	setTimeout(function(){
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            $("#c" + i + "" + j).text(board[i][j] == false ? playerIcon : board[i][j] === true ? compIcon : "");
-					if(board[i][j] !== null){
-						$("#c" + i + "" + j).prop("disabled", true);
-					}
-        }
-    }
-	}, 500);
-	
-}
-
-function makeMove() {
-    board = minimaxMove(board);
-    console.log(numNodes);
-    myMove = false;
-    setTimeout(function(){updateMove();}, 500); 
-}
-
-function minimaxMove(board) {
-    numNodes = 0;
-    return recurseMinimax(board, true)[1];
-}
-
-var numNodes = 0;
-
-function recurseMinimax(board, player) {
-    numNodes++;
-    var winner = getWinner(board);
-    if (winner !== null) {
-        switch(winner) {
-            case 1:
-                // AI wins
-                return [1, board];
-								
-            case 0:
-                // opponent wins
-                return [-1, board];
-								
-            case -1:
-                // Tie
-                return [0, board];
-								
-        }
-    } else {
-        // Next states
-        var nextVal = null;
-        var nextBoard = null;
-        
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                if (board[i][j] === null) {
-                    board[i][j] = player;
-                    var value = recurseMinimax(board, !player)[0];
-                    if ((player && (nextVal === null || value > nextVal)) || (!player && (nextVal === null || value < nextVal))) {
-                        nextBoard = board.map(function(arr) {
-                            return arr.slice();
-                        });
-                        nextVal = value;
-                    }
-                    board[i][j] = null;
-                }
-            }
-        }
-        return [nextVal, nextBoard];
-    }
-}
-
-
-
-updateMove();
